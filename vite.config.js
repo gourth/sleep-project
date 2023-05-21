a473042bd851f308e5a17d6817d0f9046ef23adb
Awinel <benjamin.huerta.awi@gmail.com>
2023-05-21 16:56:27 -0400

    vite configuraton

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/public/index.html"),
-        cart: resolve(__dirname, "src/public/cart/index.html"),
-        checkout: resolve(__dirname, "src/public/checkout/index.html"),
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
         product1: resolve(
           __dirname,
           "src/public/product_pages/cedar-ridge-rimrock-2.html"
