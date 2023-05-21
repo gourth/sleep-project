c2138f3305d9273d76c86349a4989bf36bf03108
Awinel <benjamin.huerta.awi@gmail.com>
2023-05-21 16:52:48 -0400

    fixing bug of index

--- vite.config.js
+++ vite.config.js
@@ -8,21 +8,21 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
+        main: resolve(__dirname, "src/public/index.html"),
+        cart: resolve(__dirname, "src/public/cart/index.html"),
+        checkout: resolve(__dirname, "src/public/checkout/index.html"),
         product1: resolve(
           __dirname,
           "src/product_pages/cedar-ridge-rimrock-2.html"
         ),
-        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
+        product2: resolve(__dirname, "src/public/product_pages/marmot-ajax-3.html"),
         product3: resolve(
           __dirname,
-          "src/product_pages/northface-alpine-3.html"
+          "src/public/product_pages/northface-alpine-3.html"
         ),
         product4: resolve(
           __dirname,
-          "src/product_pages/northface-talus-4.html"
+          "src/public/product_pages/northface-talus-4.html"
         ),
       },
     },
