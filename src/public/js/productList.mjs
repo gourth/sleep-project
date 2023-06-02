e52b323ee044d84c65fe3c0c4b4533c79c5116a0
haydgraham <samuelh.graham@gmail.com>
2023-06-01 20:08:42 -0600

    changed file names to externalservices

--- src/public/js/productList.mjs
+++ src/public/js/productList.mjs
@@ -1,4 +1,4 @@
-import {getProductsByCategory } from "./productData.mjs";
+import {getProductsByCategory } from "./externalServices.mjs";
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
