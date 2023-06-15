13f325371093edf62f6b13dead80ca65a42f5d72
Awinel <benjamin.huerta.awi@gmail.com>
2023-06-14 22:19:14 -0400

    bugs

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -1,18 +1,10 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
-<<<<<<< HEAD:src/js/externalServices.mjs
 async function convertToJson(res) {
   const data = await res.json();
   if (res.ok) {
     return data;
   } else {
     throw { name: "servicesError", message: data };
-=======
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
->>>>>>> 351967d9b46b8cbf0e20d5f679e2652d2f361608:src/public/js/externalServices.mjs
   }
 }
 
@@ -37,8 +29,4 @@ export async function checkout(payload) {
     body: JSON.stringify(payload),
   };
   return await fetch(baseURL + "checkout/", options).then(convertToJson);
-<<<<<<< HEAD:src/js/externalServices.mjs
 }
-=======
-}
->>>>>>> 351967d9b46b8cbf0e20d5f679e2652d2f361608:src/public/js/externalServices.mjs
