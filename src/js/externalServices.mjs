32ba9d4517910f758a00cd8845f59e221551124d
Awinel <benjamin.huerta.awi@gmail.com>
2023-06-23 14:47:09 -0400

    team assignment08

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -1,5 +1,4 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
-// q:
 async function convertToJson(res) {
   const data = await res.json();
   if (res.ok) {
@@ -30,4 +29,30 @@ export async function checkout(payload) {
     body: JSON.stringify(payload),
   };
   return await fetch(baseURL + "checkout/", options).then(convertToJson);
+}
+
+export async function loginRequest(user) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  };
+  const response = await fetch(baseURL + "login", options).then(convertToJson);
+  return response.accessToken;
+}
+// make a request to the server for the current orders
+// requires: a valid token
+// returns: a list of orders
+export async function getOrders(token) {
+  const options = {
+    method: "GET",
+    // the server will reject our request if we don't include the Authorization header with a valid token!
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await fetch(baseURL + "orders", options).then(convertToJson);
+  return response;
 }
