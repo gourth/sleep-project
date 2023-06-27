c54b2bbfbe7b5a98afe068e55be64ad52e9bba82
Awinel <benjamin.huerta.awi@gmail.com>
2023-06-27 14:34:49 -0400

    a

--- src/js/removeItem.js
+++ src/js/removeItem.js
@@ -1,3 +1,10 @@
+import { getLocalStorage } from "./utils.mjs";
+
+export function removeItem(product) {
+  const removeButton = document.querySelector(".remove-item");
+  if (removeButton) {
+    removeButton.addEventListener("click", () => {
+      const items = getLocalStorage("so-cart");
 
       if (product !== -1) {
         items.splice(product, 1);
